01c9680fee6d94655722928ca8d9a7171c266544
danhnlc <danhnlc252k@gmail.com>
2022-06-04 20:40:35 +0700

    first commit for project 3

---
+++ set_env.sh
@@ -0,0 +1,21 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=asdfghjkl123
+export POSTGRES_HOST=cdr.cq6mphs6s1uz.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=udagram-913427489295-bucket
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=project3
+export URL=http://localhost:8100
+
+# setx POSTGRES_USERNAME postgres
+# setx POSTGRES_PASSWORD asdfghjkl123
+# setx POSTGRES_HOST cdr.cq6mphs6s1uz.us-east-1.rds.amazonaws.com
+# setx POSTGRES_DB postgres
+# setx AWS_BUCKET udagram-913427489295-bucket
+# setx AWS_REGION us-east-1
+# setx AWS_PROFILE default
+# setx JWT_SECRET project3
+# setx URL http://localhost:8100
